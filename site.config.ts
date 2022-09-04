75c3a53afb525809c81ab85ab72abb9f0d84336b
Allanperrottet <100269396+Allanperrottet@users.noreply.github.com>
2022-09-04 11:38:51 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Cape Town Bucket List',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Sarah & Allan Perrottet',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
